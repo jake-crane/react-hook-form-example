a830be5eef5d04fa8be80b016c5acc24002e12f4
Jake Crane <jakecrane91@gmail.com>
2023-05-28 09:12:36 -0500

    Modified eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,12 +3,29 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'unused-imports'],
   rules: {
     'react-refresh/only-export-components': 'warn',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-wrap-multilines': ['warn', { return: 'parens-new-line' }],
+    'react/button-has-type': ['warn'],
+    'react/jsx-first-prop-new-line': ['warn'],
+    'react/jsx-max-props-per-line': ['warn', { 'maximum': 1, 'when': 'multiline' }],
+    'react/jsx-closing-bracket-location': ['warn'],
+    'semi': [1, 'always'],
+    'quotes': [1, 'single'],
+    'comma-dangle': ['warn', 'never'],
+    '@typescript-eslint/consistent-type-imports': ['warn', { prefer: 'type-imports' }],
+    'jsx-quotes': ['warn']
   },
-}
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+};
